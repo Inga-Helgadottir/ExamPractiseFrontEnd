9d61243d866c45e5c0190f80aab5bb02b0332de8
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-06-10 14:16:48 +0200

    added changes

---
+++ TODO.md
@@ -0,0 +1,6 @@
+CRUD to match endpoint in backend practise
+- remember to add with token access admin
+showHide
+favicon
+add links
+get from API
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,22 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route
+        // default for when the link is wrong
+        // like this http://localhost:3000/hjklhjklh
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <h1>There's nothing here!</h1>
+            <p>
+              <a href="/">To go back click here!</a>
+            </p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
 );
