d4aa66164bef31534b9c180c29fa9ef53b7ecce9
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-06-12 10:44:44 +0200

    links, localStorage, ShowHide, nav

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+import FetchCreateForm from "./components/FetchCreateForm";
+import FetchRead from "./components/FetchRead";
+import FetchUpdate from "./components/FetchUpdate";
+import FetchDelete from "./components/FetchDelete";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}></Route>
+      <Route path="/" element={<App />}>
+        <Route path="/Create" element={<FetchCreateForm />}></Route>
+        <Route path="/Read" element={<FetchRead />}></Route>
+        <Route path="/Update" element={<FetchUpdate />}></Route>
+        <Route path="/Delete" element={<FetchDelete />}></Route>
+      </Route>
       <Route
         // default for when the link is wrong
         // like this http://localhost:3000/hjklhjklh
